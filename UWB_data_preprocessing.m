58744d9c56362b341a721fd681e7345250c5c3f2
Minsun Kim <75118893+mins-n@users.noreply.github.com>
2022-09-14 00:53:16 +0900

    modify preprocessing code

--- UWB_data_preprocessing.m
+++ UWB_data_preprocessing.m
@@ -7,24 +7,23 @@ file_name = './Data/0830_광진,윤곤/0830_2nd_광진(1m)_윤곤(1.5m)/xethru_d
 fid_raw = fopen(file_name,'r');
 
 sample_count = 0;
-sample_drop_period = 434;
+sample_drop_period = 434; %% 이 이상 거리는 사용 안한다
 DataCursor = 1;
 rawdata = [];
 InputData = [];
+
 while (1)
     id = fread(fid_raw,1,'uint32');
     if length(id) < 1, break; end
     loop_cnt = fread(fid_raw,1,'uint32');
     numCountersFromFile = fread(fid_raw,1,'uint32');
-    Data = fread(fid_raw,[1 numCountersFromFile],'real*4');
-    %             fprintf('%d, %d, %d\n', id, loop_cnt, numCountersFromFile)
-
+    fInputData = fread(fid_raw,[1 numCountersFromFile],'single');
     sample_count = sample_count + 1;
+
     if mod(sample_count, sample_drop_period) == 0
         continue;
     end
 
-    fInputData = single(Data);
     InputData(:,DataCursor)= double(fInputData); % Raw data
     DataCursor = DataCursor + 1;
 
