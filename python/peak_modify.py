cf83373e67f32d41f8831fd9a64210b1a67f8b88
Minsun Kim <75118893+mins-n@users.noreply.github.com>
2023-04-06 19:19:29 +0900

    Update peak_modify.py

--- python/peak_modify.py
+++ python/peak_modify.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import Peak_Detection
 
-BIOPAC_folder_path= './../Data/2022.12.26/2022.12.26_2_sun_gu/4/'
+BIOPAC_folder_path= './../Data/2023.01.18/2023.01.18_2_goo_gon/4/'
 BIOPAC_path = BIOPAC_folder_path+"BIOPAC_data.npy" 
 
 
@@ -55,29 +55,22 @@ for i in range(length):
 
     data1_rpeak_i, data1_env_peak_i = Peak_Detection.Peak_Detection(data1, data1_fs, MI)#peak 적용
 
-    #피크 확인용
-    
+    # 피크 확인
+    ###
     data1_rpeak_i = np.array(
-        [13398,35638, 56881, 77484, 96371,
-         121048, 143732, 163312, 184329,
-         204266, 224214]
+        [1434, 4810, 8057, 11679, 15274,
+         18435, 21397, 24484, 27416, 30407,
+         33272, 36384, 39435, 42483,
+         45408, 48585, 52071, 55349,
+         58871]
     )
+    ###
 
     data1_rpeak_i=data1_rpeak_i.astype(int)
     data1_rpeak_i = data1_rpeak_i[data1_rpeak_i>i*size]
     data1_rpeak_i = data1_rpeak_i[data1_rpeak_i<(i+1)*size-1]
     plt.plot(data1_rpeak_i, data1[data1_rpeak_i], 'ro')
 plt.show()
-
-#저장용
-
-data1_rpeak_i = np.array(
-    [13398, 35638, 56881, 77484, 96371,
-     121048, 143732, 163312, 184329,
-     204266, 224214]
-)
-
-np.save(BIOPAC_folder_path+'data1_rpeak_i', data1_rpeak_i)
 
 
 size = len(data2)//length
@@ -91,10 +84,20 @@ for i in range(length):
     plt.xlim([i*size , (i+1)*size-1])
     plt.plot(data2)
     data2_rpeak_i, data1_env_peak_i = Peak_Detection.Peak_Detection(data2, data2_fs, MI)
-    
-    """data2_rpeak_i = np.array([7668,  18748,  30700,  41680,  53052,  64087,  73650,  84170, 93890, 103095,
- 111956, 121413, 130519, 139528, 148201, 157789, 168006, 178204, 188761,
- 198558, 208260, 216461, 224838, 232940])"""
+
+    # 피크 확인
+    ###
+    data2_rpeak_i = np.array(
+        [1194, 3778, 6875, 9556,
+         12078, 15625, 18178, 21273,
+         24083, 27403, 32020,
+         34849, 40565, 44975,
+         48195, 51107,
+         53914, 56499, 59281,
+         ]
+
+    )
+    ###
 
     data2_rpeak_i = data2_rpeak_i.astype(int)
     data2_rpeak_i = data2_rpeak_i[data2_rpeak_i>i*size]
@@ -103,7 +106,7 @@ for i in range(length):
 plt.show()
 
 
-# data2_rpeak_i = np.array([7668,  18748,  30700,  41680,  53052,  64087,  73650,  84170, 93890, 103095,
-#  111956, 121413, 130519, 139528, 148201, 157789, 168006, 178204, 188761,
-#  198558, 208260, 216461, 224838, 232940])
-# np.save(BIOPAC_folder_path +'data2_rpeak_i', data2_rpeak_i)
+#저장
+
+np.save(BIOPAC_folder_path+'data1_rpeak_i', data1_rpeak_i)
+np.save(BIOPAC_folder_path +'data2_rpeak_i', data2_rpeak_i)
