3e4e6ef7b550cfa582ff6c813762b0b0a08641c2
Minsun Kim <75118893+mins-n@users.noreply.github.com>
2023-04-12 20:28:06 +0900

    Update program.py

--- python/program.py
+++ python/program.py
@@ -7,81 +7,10 @@ import warnings
 import neurokit2 as nk
 import matplotlib.pyplot as plt
 import tensorflow as tf
+import util
 
 warnings.filterwarnings("ignore")
 
-
-def gradients(I):
-    N = len(I)
-    M = len(I[0])
-
-    Ix = np.zeros((N, M))
-    Iy = np.zeros((N, M))
-
-    for i in range(N):
-        for j in range(M):
-            if i < N:
-                Ix[i - 1, j - 1] = I[i, j - 1] - I[i - 1, j - 1]
-            else:
-                Ix[N - 1, j - 1] = I[0, j - 1] - I[N - 1, j - 1]
-
-            if j < M:
-                Iy[i - 1, j - 1] = I[i - 1, j] - I[i - 1, j - 1]
-            else:
-                Iy[i - 1, M - 1] = I[i - 1, 0] - I[i - 1, M - 1]
-
-    return Ix, I
-
-
-def l0_grad_minimization(y, L):
-    N = len(y)
-    M = len(y[0])
-
-    u = 1.0 * y
-
-    h = np.zeros((N, M))
-    v = np.zeros((N, M))
-
-    p = np.zeros((N, M))
-    q = np.zeros((N, M))
-
-    fy = np.zeros((N, M))
-    fy = np.fft.fft2(y)
-
-    dx = np.zeros((N, M))
-    dy = np.zeros((N, M))
-    dx[1, 1] = -1.0
-    dx[N - 1, 1] = 1.0
-    dy[1, 1] = -1.0
-    dy[1, M - 1] = 1.0
-
-    fdxt = np.tile(np.conj(np.fft.fft2(dx)), [1, 1])
-    fdyt = np.tile(np.conj(np.fft.fft2(dy)), [1, 1])
-
-    adxy = abs(fdxt) ** 2 + abs(fdyt) ** 2
-
-    beta = 0.5 / L
-    for t in range(50):
-        if beta <= 1e-2:
-            break
-
-        [ux, uy] = gradients(u)
-
-        ls = abs(ux) ** 2 + abs(uy) ** 2 >= beta * L
-        h = ls * ux
-        v = ls * uy
-
-        fh = np.zeros((N, M))
-        fv = np.zeros((N, M))
-        fh = np.fft.fft2(h)
-        fv = np.fft.fft2(v)
-
-        fu = (beta * fy + (fdxt * fh + fdyt * fv)) / (beta + adxy)
-        u = np.real(np.fft.ifft2(fu))
-
-        beta = 0.65 * beta
-
-    return u, h, v
 
 model = tf.keras.models.load_model("./respiration_rate_predict")
 
@@ -245,7 +174,7 @@ for n in range(Human_cnt):
     im = rawdata[int(Distance[n, 0]):int(Distance[n, 1]) + 1, :]
 
     L = 0.02
-    [u, ux, uy] = l0_grad_minimization(im, L)
+    [u, ux, uy] = util.l0_grad_minimization(im, L)
 
     Scharr_dx = cv2.Scharr(u.astype('float32'), -1, 1, 0)
     Scharr_dy = cv2.Scharr(u.astype('float32'), -1, 0, 1)
