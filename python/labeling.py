6740f13fcf195f9b21ff6bb9c935925a8fd8f4cb
Minsun Kim <75118893+mins-n@users.noreply.github.com>
2023-04-18 18:25:50 +0900

    Update labeling.py
    파일 이름 변경 및 window size 1분으로 수정

--- python/labeling.py
+++ python/labeling.py
@@ -1,17 +1,24 @@
 from PIL import Image
 import numpy as np
 import os
+import re
 
 window_size = 10
 uwb_fs = 20
 root_dir = "./../Data/"
 
-def get_img_path(root_dir):
+def get_gray_path(root_dir):
     file_list = []
     for (root, dirs, files) in os.walk(root_dir):
         for file_name in files:
             if (file_name.endswith("_gray.jpg")):
                 file_list.append(root + "/" + file_name)
+    return file_list
+
+def get_RGB_path(root_dir):
+    file_list = []
+    for (root, dirs, files) in os.walk(root_dir):
+        for file_name in files:
             if (file_name.endswith("_person.jpg")):
                 file_list.append(root + "/" + file_name)
     return file_list
@@ -32,17 +39,38 @@ def get_peak_path(root_dir):
                 file_list.append(root + "/" + file_name)
     return file_list
 
-def generate_img_dataset():
-    img_path_list = get_img_path(root_dir)
+def generate_gray_dataset():
+    img_path_list = get_gray_path(root_dir)
     for img_path in img_path_list:
         img = []
         load_img = Image.open(img_path).convert("L")
         # print(load_img)
         img_data = np.array(load_img)
-        for j in range(110):
-            tmp = img_data[:, j * uwb_fs:(10 + j) * uwb_fs]
+        for j in range(0,uwb_fs*61,uwb_fs):
+            tmp = img_data[:, j:j+uwb_fs*60]
             img.append(tmp)
-        np.save(img_path[:-4] + ".npy",img)
+        filename = os.path.basename(img_path)
+        match = re.search(r'\d+', filename)
+        num = int(match.group())
+        parent_dir = os.path.dirname(img_path)
+        np.save(parent_dir + f"/{num}_person_gray.npy",img)
+    return "complete"
+
+def generate_RGB_dataset():
+    img_path_list = get_RGB_path(root_dir)
+    for img_path in img_path_list:
+        img = []
+        load_img = Image.open(img_path)
+        # print(load_img)
+        img_data = np.array(load_img)
+        for j in range(0,uwb_fs*61,uwb_fs):
+            tmp = img_data[:, j:j+uwb_fs*60]
+            img.append(tmp)
+        filename = os.path.basename(img_path)
+        match = re.search(r'\d+', filename)
+        num = int(match.group())
+        parent_dir = os.path.dirname(img_path)
+        np.save(parent_dir + f"/{num}_person_RGB.npy", img)
     return "complete"
 
 def generate_UWB_dataset():
@@ -50,10 +78,14 @@ def generate_UWB_dataset():
     for UWB_path in UWB_path_list:
         UWB = []
         UWB_data = np.load(UWB_path)
-        for j in range(110):
-            tmp = UWB_data[j * uwb_fs:(10 + j) * uwb_fs]
+        for j in range(0,uwb_fs*61,uwb_fs):
+            tmp = UWB_data[j : j + uwb_fs*60]
             UWB.append(tmp)
-        np.save(UWB_path[:-4] + "_input.npy", UWB)
+        filename = os.path.basename(UWB_path)
+        match = re.search(r'\d+', filename)
+        num = int(match.group())
+        parent_dir = os.path.dirname(UWB_path)
+        np.save(parent_dir + f"/{num}_person_UWB_1D.npy", UWB)
     return "complete"
 def generate_RR_ref():
     ref_path_list = get_peak_path(root_dir)
@@ -65,42 +97,20 @@ def generate_RR_ref():
         dir__ = os.path.dirname(ref_path)
         dir__ = dir__ + "\\BIOPAC_data.npy"
         biopac_fs = np.load(dir__,allow_pickle=True)[1]
-        for j in range(110):
+        for j in range(0,biopac_fs*61,biopac_fs):
             tmp = load_ref
-            tmp = tmp[tmp > j * biopac_fs]
-            tmp = tmp[tmp < (10 + j) * biopac_fs]
-            if len(tmp) <= 1:
-                k = 1
-                while len(tmp) < 2:
-                    tmp = load_ref
-                    tmp = tmp[tmp > (j-k) * biopac_fs]
-                    tmp = tmp[tmp < (10 + j + k) * biopac_fs]
-                    k = k + 1
-            diffs = np.diff(tmp)
-            tmp_diffs = np.mean(diffs)
-            RR = round(60/(tmp_diffs/biopac_fs),2)
+            tmp = tmp[tmp >= j]
+            tmp = tmp[tmp <= j+biopac_fs*60]
+            RR = len(tmp)
             ref_list.append(RR)
-        # print(ref_list)
-        np.save(ref_path[:-12] + "_ref.npy",ref_list)
-    return "complete"
-def generate_dot_ref():
-    ref_path_list = get_peak_path(root_dir)
-    for ref_path in ref_path_list:
-        ref_list = []
-        load_ref = np.load(ref_path)
-        load_ref = np.sort(load_ref)
-        load_ref = np.unique(load_ref)
-        dir__ = os.path.dirname(ref_path)
-        dir__ = dir__ + "\\BIOPAC_data.npy"
-        biopac_fs = np.load(dir__,allow_pickle=True)[1]
-        for j in range(110):
-            tmp = load_ref
-            tmp = tmp[tmp > j * biopac_fs]
-            tmp = tmp[tmp < (10 + j) * biopac_fs]
-            ref_list.append(len(tmp))
-        np.save(ref_path[:-12] + "_ref.npy",ref_list)
+        filename = os.path.basename(ref_path)
+        match = re.search(r'\d+', filename)
+        num = int(match.group())
+        parent_dir = os.path.dirname(ref_path)
+        np.save(parent_dir + f"/{num}_person_label.npy", ref_list)
     return "complete"
 
-print(generate_img_dataset())
+print(generate_gray_dataset())
+print(generate_RGB_dataset())
 print(generate_UWB_dataset())
 print(generate_RR_ref())
